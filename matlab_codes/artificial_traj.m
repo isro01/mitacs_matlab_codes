d2aed3513fef0d495147dee485d71f5fbdc83181
Ishir Roongta <ishir@iitk.ac.in>
2022-06-28 15:46:54 -0400

    Minor changes

--- matlab_codes/artificial_traj.m
+++ matlab_codes/artificial_traj.m
@@ -2,7 +2,7 @@
 r=1;
 % th = 0:pi/50:2*pi;
 t = linspace(0, 10,100);
-xunit = r * cos(th) + 0;
-yunit = r * sin(th) + 0;
+xunit = r * cos(t) + 0;
+yunit = r * sin(t) + 0;
 h = plot(xunit, yunit);
 axis([-2 2 -2 2])
