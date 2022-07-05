30a197e5714bdc47545c5926fbbd8f83f30dd180
Ishir Roongta <ishir@iitk.ac.in>
2022-07-05 14:22:23 -0400

    Add older version and circle trajectory

--- matlab_codes/main.m
+++ matlab_codes/main.m
@@ -19,11 +19,11 @@ a1(1);
 
 k11 = 1; k21 = 1;k12 = 1;k22 = 1;
 
-% p1 = [1;0.5;0]; p2 = [3;1;0];
-% d11 = norm(p1-a1);
-% d13 = norm(p1-a3);
-% d22 = norm(p2-a2);
-% d23 = norm(p2-a3);
-% q12 = norm(p1-p2);
-% q21 = norm(p1-p2);
+p1 = [1;0.5;0]; p2 = [3;1;0];
+d11 = norm(p1-a1);
+d13 = norm(p1-a3);
+d22 = norm(p2-a2);
+d23 = norm(p2-a3);
+q12 = norm(p1-p2);
+q21 = norm(p1-p2);
 
