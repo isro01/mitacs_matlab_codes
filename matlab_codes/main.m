8792451f6890f9e6e039f31fc0c8b7ac8e84e855
Ishir Roongta <ishir@iitk.ac.in>
2022-07-05 20:46:55 -0400

    Rework model and correct velocity term

--- matlab_codes/main.m
+++ matlab_codes/main.m
@@ -15,15 +15,13 @@ a3 = params.anchor3;
 % q12 = params.q12;
 % q21 = params.q21;
 
-a1(1);
-
-k11 = 1; k21 = 1;k12 = 1;k22 = 1;
-
-p1 = [1;0.5;0]; p2 = [3;1;0];
-d11 = norm(p1-a1);
-d13 = norm(p1-a3);
-d22 = norm(p2-a2);
-d23 = norm(p2-a3);
-q12 = norm(p1-p2);
-q21 = norm(p1-p2);
+%%Uncomment the below code and disconnect the p1 and p2 inputs in simulink
+%%to run for custom hover positions
+% p1 = [1;0.5;0]; p2 = [3;1;0];
+% d11 = norm(p1-a1);
+% d13 = norm(p1-a3);
+% d22 = norm(p2-a2);
+% d23 = norm(p2-a3);
+% q12 = norm(p1-p2);
+% q21 = norm(p1-p2);
 
